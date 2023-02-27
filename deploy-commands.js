7f574927e93acf871967664c362c98bdf73ed5b6
Chokun Rojanapron <chokun@chokunplayz.com>
2023-02-27 19:24:23 +0700

    tify up deploy commands and switch to async func

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,32 +1,36 @@
 // I HAVE NO IDEA WHAT THIS CODE DOES, I MADE IT AT LIKE MIDNIGHT AND I DON'T KNOW WHAT IT DOES, I NEED IT THOUGH
 // Figured it out now, Thank you winword.zip for the help
 
-require('dotenv').config()
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST, Routes } = require('discord.js');
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST, Routes } = require("discord.js");
 
-function deployCommands(){
+async function deployCommands() {
     const commands = [];
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    const commandFiles = fs
+        .readdirSync("./commands")
+        .filter((file) => file.endsWith(".js"));
     for (const file of commandFiles) {
         const command = require(`./commands/${file}`);
         commands.push(command.data.toJSON());
     }
-    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-    (async () => {
-        try {
-            console.log(`Started refreshing ${commands.length} application (/) commands.`);
-            const data = await rest.put(
-                Routes.applicationCommands(process.env.clientId),
-                { body: commands },
-            );
-    
-            console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-        } catch (error) {
-            console.error(error);
-        }
-    })();
+    const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+    try {
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`
+        );
+        const data = await rest.put(
+            Routes.applicationCommands(process.env.clientId),
+            { body: commands }
+        );
+
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`
+        );
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = deployCommands;

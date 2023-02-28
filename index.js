b95fdf24e56af26e588fe144b058372ea4bf4dda
Chokun Rojanapron <chokun@chokunplayz.com>
2023-02-28 08:44:53 +0700

    fix typo, fix missing closing }, add comments and removing token from the code

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Require the necessary discord.js classes
 
-const { Client, GatewayIntentBits, REST, Routes, ActivityType, Collection, Events } = require('discord.js');
-const dotenv = require('dotenv');
-const fs = require('fs');
-const fetch = require('@replit/node-fetch');
-const path = require('path');
-const cron = require('node-cron');
-const keepAlive = require('./server.js');
-const deployCommands = require('./deploy-commands.js');
+const {
+	Client,
+	GatewayIntentBits,
+	REST,
+	Routes,
+	ActivityType,
+	Collection,
+	Events,
+} = require("discord.js");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const fetch = require("@replit/node-fetch");
+const path = require("path");
+const cron = require("node-cron");
+const keepAlive = require("./server.js");
+const deployCommands = require("./deploy-commands.js");
 
 // loads .env file as enviorment varibles
 require("dotenv").config();
@@ -28,27 +36,26 @@ client.once(Events.ClientReady, async () => {
 	client.user.setActivity("with Chitose", { type: ActivityType.Playing });
 	client.user.setStatus("online");
 	await deployCommands();
-	
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith(".js"));
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ("data" in command && "execute" in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(
-			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-		);
+
+	const commandsPath = path.join(__dirname, "commands");
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".js"));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ("data" in command && "execute" in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
 	}
-}
-client.on(Events.guildCreate, (c) => {
-	console.log(`Joined new guild: ${c.name}`);
-	updateCommands();
 });
+
+// On Interaction, commands, button, etc do:
 client.on(Events.InteractionCreate, async (interaction) => {
 	const command = client.commands.get(interaction.commandName);
 	if (!command) {
@@ -66,32 +73,25 @@ client.on(Events.InteractionCreate, async (interaction) => {
 	}
 });
 
+// On Joining a new Server do:
 client.on(Events.guildCreate, (guild) => {
 	console.log(`Added to new server: ${guild.name}`);
 });
+// On Leaving a Server do:
 client.on(Events.guildDelete, (guild) => {
 	console.log(`Removed from server: ${guild.name}`);
 });
 
-cron.schedule('*/5 * * * *', async function() {
-    console.log("Pinging HuggingFace API");
-		async function query(data) {
-		const response = await fetch(
-			"https://api-inference.huggingface.co/models/Hobospider132/DialoGPT-Mahiru-Proto",
-			{
-				method: "POST",
-				body: JSON.stringify(data),
-				headers: { Authorization: "Bearer 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" } // HTOKEN is my API key for HuggingFace
-			}
-		);
-		const result = await response.json();
-		return result;
-	}
-	query({
-		"inputs": {
-			"text": "Hello!"
-		}
-	}).then((response) => {
-		console.log(JSON.stringify(response));
-	});
+// Ping Hugging Face API every 5 minutes using node-cron
+cron.schedule("*/5 * * * *", async function () {
+	console.log("Pinging HuggingFace API");
+	await huggingface
+		.query({
+			inputs: {
+				text: prompt,
+			},
+		})
+		.then((response) => {
+			console.log(JSON.stringify(response));
+		});
 });

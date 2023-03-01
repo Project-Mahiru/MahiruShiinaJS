5f12fd266007d4382316534e907da7600f31d207
Hobospider132 <100825450+Hobospider132@users.noreply.github.com>
2023-03-01 17:21:47 +1100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const server = express()
+
+server.all("/", (req, res) => {
+	res.send("Bot is online!")
+})
+
+function keepAlive() {
+	server.listen(3000, () => {
+		console.log("Server is online!")
+	})
+}
+
+module.exports = keepAlive

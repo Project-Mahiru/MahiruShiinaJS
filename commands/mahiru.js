d0ab52946daa01d84f0b02a5a7c59f63c9501684
Chokun Rojanapron <chokun@chokunplayz.com>
2023-03-15 05:29:20 +0700

    improved error handling on mahiru command and huggingface libary

--- commands/mahiru.js
+++ commands/mahiru.js
@@ -13,12 +13,22 @@ module.exports = {
 	async execute(client, interaction) {
 		await interaction.deferReply();
 
-		const prompt = interaction.options.getString('prompt');
+		try {
+			const prompt = interaction.options.getString('prompt');
 
-		const data = await query(prompt);
+			const data = await query(prompt);
 
-		let botResponse = data;
-		// send message to channel as a reply
-		await interaction.editReply(`\`\`\`Input was: ${prompt}\`\`\`${botResponse}`.replace(/<@[^>]+>/g, '<removed>'));
+			let botResponse = data;
+			// send message to channel as a reply
+			await interaction.editReply(`\`\`\`Input was: ${prompt}\`\`\`${botResponse}`.replace(/<@[^>]+>/g, '<removed>'));
+		} catch (error) {
+			console.error(error);
+			if (error.message.includes("ECONNREFUSED")) {
+			  await interaction.editReply("Looks like the mahiru is sleeping, please come back when she's awake.");
+			} else {
+			  await interaction.editReply(`An error occurred: ${error.message}`);
+			}
+		  }
+		  
 	}
 };

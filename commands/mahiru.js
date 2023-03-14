d663f9111ee5cac4ebd74887706ba8856da6831b
Chokun Rojanapron <chokun@chokunplayz.com>
2023-03-15 05:17:39 +0700

    added deferreply to mahiru command to prevent 'app does not respond' error

--- commands/mahiru.js
+++ commands/mahiru.js
@@ -11,12 +11,14 @@ module.exports = {
         .setDescription('Enter a prompt')
         .setRequired(true)),
 	async execute(client, interaction) {
+		await interaction.deferReply();
+
 		const prompt = interaction.options.getString('prompt');
 
 		const data = await query(prompt);
 
 		let botResponse = data;
 		// send message to channel as a reply
-		await interaction.reply(`\`\`\`Input was: ${prompt}\`\`\`${botResponse}`.replace(/<@[^>]+>/g, '<removed>'));
+		await interaction.editReply(`\`\`\`Input was: ${prompt}\`\`\`${botResponse}`.replace(/<@[^>]+>/g, '<removed>'));
 	}
 };
